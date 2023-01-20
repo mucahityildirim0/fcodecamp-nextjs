2ece246a3c16d195c01bc91958f511a46e5ec724
Mücahit Yıldırım <mucahityildirim@Mucahit-MacBook-Air.local>
2023-01-20 14:37:40 +0300

    nextjs build pages

---
+++ pages/about-us.js
@@ -0,0 +1,17 @@
+const Page = () => {
+  return (
+    <div>
+      <h1>About us Page</h1>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum quasi
+      excepturi libero quisquam sapiente! Ipsa mollitia, quas perferendis
+      asperiores ducimus quia tenetur sed dicta animi officia voluptatum at
+      assumenda sit debitis corrupti, ratione quidem qui, alias atque obcaecati
+      porro delectus illum totam vitae. Adipisci maxime culpa nihil natus ut
+      ipsam fugiat. Atque doloribus possimus sapiente. Earum molestiae vero
+      cupiditate at dicta commodi nulla eius necessitatibus eveniet. Deserunt,
+      porro! Debitis, saepe.
+    </div>
+  );
+};
+
+export default Page;
---
+++ pages/events/index.js
@@ -0,0 +1,23 @@
+const Page = () => {
+  return (
+    <div>
+      <h1>Events Page</h1>
+      <div>
+        <a>
+          <img />
+          <h2>Events in London</h2>
+        </a>
+        <a>
+          <img />
+          <h2>Events in San Francisco</h2>
+        </a>
+        <a>
+          <img />
+          <h2>Events in Barcelona</h2>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
